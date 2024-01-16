e28bbb128894ccc40df26eb773ac0570fe2f0c8a
Ayari-Ahmed <ayariayari954@gmail.com>
2024-01-16 16:08:46 +0100

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Topics from "./pages/bac-topics";
+import Print from "./pages/print-topics";
+import { MantineProvider } from "@mantine/core";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider>
+      <Router>
+        
+        <Routes>
+        <Route path="/" element={<Home />} index />
+          <Route path="/bac-topics" element={<Topics />} />
+          <Route path="/print-topics" element={<Print />} />
+        </Routes>
+      </Router>
+    </MantineProvider>
   );
-}
+};
 
 export default App;
---
+++ src/pages/bac-topics.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BacTopics() {
+  return (
+    <div>
+      <h1>مواضيع البكالوريا</h1>
+      {/* Add your content for the BAC Topics page */}
+    </div>
+  );
+}
+
+export default BacTopics;
