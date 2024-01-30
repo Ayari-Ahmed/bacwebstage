eb82640a503b0ecfc0ccd685c9835d93aaa59874
Ayari-Ahmed <ayariayari954@gmail.com>
2024-01-30 17:59:43 +0100

    added login panel connected to the database for admin to reach the Dashboard pages

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,11 +3,7 @@ import "./DashBoard.css";
 import { Grid } from "@mantine/core";
 import Logo1 from "../assets/logo1.png";
 import Tun from "../assets/tun.png";
-import {
-  BrowserRouter as Router,
-  Link,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Link, useLocation } from "react-router-dom";
 import {
   IconHome,
   IconDatabaseImport,
@@ -38,56 +34,67 @@ export default function DashBoard() {
                 <div className={classes.header}>
                   <img src={Logo1} alt="Logo1" className={classes.Logo1} />
                 </div>
-                <button
-                  className={classes.link}
-                  data-active={active === "Home" || undefined}
-                  onClick={() => setActive("Home")}
-                >
-                  <IconHome className={classes.linkIcon} stroke={1.5} />
-                  <span>Home</span>
-                </button>
-                <Link
-                  className={classes.link}
-                  data-active={active === "Databases" || undefined}
-                  onClick={() => {
-                    setActive("Databases");
-
-                    handleLinkClick(
-                      "http://localhost/phpmyadmin/index.php"
-                    );
-                  }}
-                >
-                  <IconDatabaseImport
-                    className={classes.linkIcon}
-                    stroke={1.5}
-                  />
-                  <span>Databases</span>
-                </Link>
-                <Link
-                  className={classes.link}
-                  data-active={active === "Sections" || undefined}
-                  onClick={() => {
-                    setActive("Sections");
-                    handleLinkClick("/DashBoard/Sections");
-                  }}
-                >
-                  <IconBoxMultiple className={classes.linkIcon} stroke={1.5} />
-                  <span>Sections</span>
-                </Link>
-                <Link
-                  className={classes.link}
-                  data-active={active === "Matieres" || undefined}
-                  onClick={() => {
-                    setActive("Matieres");
-                    handleLinkClick("/DashBoard/Matieres");
-                  }}
-                >
-                  <IconClipboardData
-                    className={classes.linkIcon}
-                    stroke={1.5}
-                  />
-                  <span>Matières</span>
-                </Link>
+                <ul>
+                  <li>
+                    <button
+                      className={classes.link}
+                      data-active={active === "Home" || undefined}
+                      onClick={() => {
+                        setActive("Home");
+                        handleLinkClick("/");
+                      }}
+                    >
+                      <IconHome className={classes.linkIcon} stroke={1.5} />
+                      <span>Home</span>
+                    </button>
+                  </li>
+                  <li>
+                    <Link
+                      className={classes.link}
+                      
+                    >
+                      <IconDatabaseImport
+                        className={classes.linkIcon}
+                        stroke={1.5}
+                      />
+                      <span>Databases</span>
+                    </Link>
+                    <ul>
+                      <li>
+                        <Link
+                          className={classes.link}
+                          data-active={active === "Sections" || undefined}
+                          onClick={() => {
+                            setActive("Sections");
+                            handleLinkClick("/DashBoard/Sections");
+                          }}
+                        >
+                          <IconBoxMultiple
+                            className={classes.linkIcon}
+                            stroke={1.5}
+                          />
+                          <span>Sections</span>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          className={classes.link}
+                          data-active={active === "Matieres" || undefined}
+                          onClick={() => {
+                            setActive("Matieres");
+                            handleLinkClick("/DashBoard/Matieres");
+                          }}
+                        >
+                          <IconClipboardData
+                            className={classes.linkIcon}
+                            stroke={1.5}
+                          />
+                          <span>Matières</span>
+                        </Link>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
               </div>
 
               <div className={classes.footer}>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./DashBoard.css";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -29,30 +31,46 @@ const Login = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-    <div>
-      <label htmlFor="username">Username:</label>
-      <input
-        type="text"
-        id="username"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
+    <div id="Login_Box">
+      <form onSubmit={handleSubmit}>
+        <h1>Login Admin </h1>
+        <br />
+        <table className="Table_Log">
+          <tr >
+            <td>
+              <h2>Username :</h2>
+            </td>
+            <td>
+              <input
+                type="text"
+                id="username"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h2>Password :</h2>
+            </td>
+            <td>
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </td>
+          </tr>
+          <div></div>
+          <div></div>
+        </table>
+        {error && <div style={{ color: "red" }}>{error}</div>}
+        <Button type="submit" variant="outline-success">Login</Button>
+      </form>
     </div>
-    <div>
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        id="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-    </div>
-    {error && <div style={{ color: "red" }}>{error}</div>}
-    <button type="submit">Login</button>
-  </form>
   );
 };
 
